33ef5246ed1038640256f5e9d0e85b189ced7461
akshat-2502 <khulbeakshat@gmail.com>
2025-06-27 21:40:41 +0530

    login function is created

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -44,3 +44,30 @@ export const registerUser = async (req, res) => {
     });
   }
 };
+
+// LOGIN FUNCTION
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(500).json({ message: "Invalid Credentials" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(500).json({ message: "Invalid Credentials" });
+    }
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+      error: error.message,
+    });
+  }
+};
