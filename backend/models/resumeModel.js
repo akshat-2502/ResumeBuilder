4fb11dc78df264f3dd09796d536f50e9dfc60192
akshat-2502 <khulbeakshat@gmail.com>
2025-06-28 01:09:18 +0530

    resume schema is being created

--- backend/models/resumeModel.js
+++ backend/models/resumeModel.js
@@ -1,3 +1,93 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
-const ResumeSchema = new mongoose.Schema({});
+const ResumeSchema = new mongoose.Schema(
+  {
+    userdId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    thumbnailLink: {
+      type: String,
+    },
+    template: {
+      theme: String,
+      colorPalette: [String],
+    },
+    profileInfo: {
+      profilePreviewUrl: String,
+      fullName: String,
+      designation: String,
+      summary: String,
+    },
+    contactInfo: {
+      email: String,
+      phone: String,
+      location: String,
+      linkedIn: String,
+      gitHub: String,
+      website: String,
+    },
+
+    //Work Experience Data
+
+    workExperience: [
+      {
+        comapany: String,
+        role: String,
+        startDate: String,
+        endDate: String,
+        description: String,
+      },
+    ],
+    education: [
+      {
+        degree: String,
+        institution: String,
+        startDate: String,
+        endDate: String,
+      },
+    ],
+    skills: [
+      {
+        name: String,
+        progress: Number,
+      },
+    ],
+    projects: [
+      {
+        title: String,
+        description: String,
+        github: String,
+        liveDemo: String,
+      },
+    ],
+    certifications: [
+      {
+        title: String,
+        issuer: String,
+        year: String,
+      },
+    ],
+    languages: [
+      {
+        name: String,
+        progress: String,
+      },
+    ],
+    intrests: [String],
+  },
+  {
+    timestamps: {
+      createdAt: "createAt",
+      updatedAt: "updatedAt",
+    },
+  }
+);
+
+const Resume = mongoose.model("Resume", ResumeSchema);
+export default Resume;
