b7b8b45f4f0694eaad19b9b16d6c00549b167f9d
akshat-2502 <khulbeakshat@gmail.com>
2025-06-29 00:49:28 +0530

    frontend in prog

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { LayoutTemplate, Menu, X } from "lucide-react";
 import { landingPageStyles } from "../assets/dummystyle.js";
+import { UserContext } from "../context/UserContext.jsx";
+import { useNavigate } from "react-router-dom";
+import { ProfileInfoCard } from "../components/Cards.jsx";
 
 const LandingPage = () => {
+  const { user } = useContext(UserContext);
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [openAuthModel, setOpenAuthModel] = useState(false);
+  const navigate = useNavigate();
   return (
     <div className={landingPageStyles.container}>
       {/* HEADER */}
@@ -28,8 +34,56 @@ const LandingPage = () => {
           </button>
 
           {/* DeskTop Navigation */}
-          <div className="hidden md:flex items-center"></div>
+          <div className="hidden md:flex items-center">
+            {user ? (
+              <ProfileInfoCard />
+            ) : (
+              <button
+                className={landingPageStyles.desktopAuthButton}
+                onClick={() => setOpenAuthModel(true)}
+              >
+                <div
+                  className={landingPageStyles.desktopAuthButtonOverlay}
+                ></div>
+                <span className={landingPageStyles.desktopAuthButtonText}>
+                  Get Started
+                </span>
+              </button>
+            )}
+          </div>
         </div>
+
+        {/* MOBILE MENU */}
+        {mobileMenuOpen && (
+          <div className={landingPageStyles.mobileMenu}>
+            <div className={landingPageStyles.mobileMenuContainer}>
+              {user ? (
+                <div className={landingPageStyles.mobileUserInfo}>
+                  <div className={landingPageStyles.mobileUserWelcome}>
+                    Welcome Back!
+                  </div>
+                  <button
+                    className={landingPageStyles.mobileDashboardButton}
+                    onClick={() => {
+                      navigate("/dashboard"), setMobileMenuOpen(false);
+                    }}
+                  >
+                    Go To Dashboard
+                  </button>
+                </div>
+              ) : (
+                <button
+                  className={landingPageStyles.mobileAuthButton}
+                  onClick={() => {
+                    setOpenAuthModel(true), setMobileMenuOpen(false);
+                  }}
+                >
+                  Get Started
+                </button>
+              )}
+            </div>
+          </div>
+        )}
       </header>
     </div>
   );
